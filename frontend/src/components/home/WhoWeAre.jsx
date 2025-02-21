6138e13afa301bd82e069a05a51526abd20affc2
VanshKirtishahi <vanshkirtishahi@gmail.com>
2025-02-21 15:24:37 +0530

    initial commit

--- frontend/src/components/home/WhoWeAre.jsx
+++ frontend/src/components/home/WhoWeAre.jsx
@@ -8,7 +8,7 @@ const WhoWeAre = () => {
           {/* Image Section */}
           <div className="relative">
             <img
-              src="/images/team/"  // Direct path to image in public folder
+              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  // Direct path to image in public folder
               alt="Support Team"
               className="rounded-lg shadow-xl w-full h-auto"
               loading="lazy"
